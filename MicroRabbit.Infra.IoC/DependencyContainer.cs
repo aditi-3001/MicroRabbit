a5842535acd8cc276df7add2055b863ace225b31
Aditi Bajpai <aditibajpai3001@gmail.com>
2020-10-02 14:29:56 +0530

    IoC Commit

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MicroRabbit.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection servcies)
+        {
+            servcies.AddTransient<IEventBus,RabbitMQBus>();
+        }
+    }
+}
